6e8d4c235483bc5de83dc11f23c6b15c18774d78
qedpi <junruihu@gmail.com>
2018-04-07 19:02:41 -0700

    mvp contract

--- dapp/contracts/Picolo.sol
+++ dapp/contracts/Picolo.sol
@@ -17,6 +17,82 @@ contract Picolo is Ownable, priced {
 
   	using SafeMath for uint256;
 
+  	   	// Todo: refactor into subcontract
+   	uint public entryStake = 4;  // amount for testing
+   	mapping(address => uint) public registeredNodesStake;
+   	mapping(address => string) public nodeStatus;
+
+   	uint public convictThreshold = 3;
+   	mapping(address => uint) public voteTally;
+   	mapping(address => uint) public maliciousVoteTally;
+   	mapping(address => bool) public evictionVoters;
+
+    // Status of transaction. Used for error handling.
+    event Status(string indexed statusCode);
+
+   	event nodeStatusChangeEvent (
+   		string indexed newStatus,
+   		address indexed sender
+   	);
+
+
+   	function registerNode() public {
+   	   	require(keccak256(nodeStatus[msg.sender]) == keccak256(""));
+   	   	nodeStatus[msg.sender] = 'registered';	
+   	   	nodeStatusChangeEvent('registered', msg.sender);
+   	}
+
+   	function sendDeposit() public payable costs(entryStake) {
+   		require(keccak256(nodeStatus[msg.sender]) == keccak256("registered"));
+   		registeredNodesStake[msg.sender] = entryStake;	
+   		nodeStatus[msg.sender] = 'valid';
+   	   	nodeStatusChangeEvent('valid', msg.sender);
+  	}
+
+   	function startMining() public {
+   		require(keccak256(nodeStatus[msg.sender]) == keccak256("valid"));
+   	   	nodeStatus[msg.sender] = 'active';	
+   	   	nodeStatusChangeEvent('active', msg.sender);
+   	}
+
+   	function stopMining() public {
+   		require(keccak256(nodeStatus[msg.sender]) == keccak256("active"));
+   	   	nodeStatus[msg.sender] = 'inactive';	
+   	   	nodeStatusChangeEvent('inactive', msg.sender);
+   	}
+
+   	function claimDeposit() public {
+   		require(keccak256(nodeStatus[msg.sender]) != keccak256("malicious"));
+   		require(keccak256(nodeStatus[msg.sender]) == keccak256("pending"));
+   		msg.sender.transfer(registeredNodesStake[msg.sender]);
+   	   	nodeStatus[msg.sender] = 'registered';	
+   	   	nodeStatusChangeEvent('registered', msg.sender);
+   	}
+
+   	function changeEntryStake(uint _price) public onlyOwner {
+        entryStake = _price;
+    }
+
+    // Challenges 
+    function addPotentialMalicious(address _target) public onlyOwner {
+    	require(keccak256(nodeStatus[msg.sender]) == keccak256("malicious"));
+   	   	nodeStatus[msg.sender] = 'pending';	
+    }
+
+    // Consensus
+    function voteMalicious(address _target, bool _convict) public {
+    	require(keccak256(nodeStatus[msg.sender]) == keccak256("pending"));
+    	voteTally[_target].add(1);
+    	if (_convict) {
+    		maliciousVoteTally[_target].add(1);
+    	}
+    	if (voteTally[_target] >= convictThreshold 
+    		&& maliciousVoteTally[_target].mul(2) > voteTally[_target]) {
+    		nodeStatus[_target] = 'malicious';	
+   	   		nodeStatusChangeEvent('malicious', msg.sender);
+    	}
+    }
+
     string  public name = "Picolo Token";
     string  public symbol = "PCT";
     string  public standard = "Picolo Token v1.0";
@@ -75,33 +151,5 @@ contract Picolo is Ownable, priced {
 
         return true;
     }
-
-   	
-
-   	// Todo: refactor into subcontract
-   	uint public entryStake = 4;  // amount for testing
-   	mapping(address => uint) public registeredNodesStake;
-
-    // Status of transaction. Used for error handling.
-    event Status(string indexed statusCode);
-
-   	event registeredEvent (address indexed sender);
-
-   	function registerNode() public payable costs(entryStake) {
-   		require(!registeredNodesStake[msg.sender]);
-   		registeredNodesStake[msg.sender] = entryStake;	
-   		registeredEvent(msg.sender);
-   	}
-
-   	function changeEntryStake(uint _price) public onlyOwner {
-        entryStake = _price;
-    }
-
-    // Challenges 
-
-    // TCR for masternodes
-}
-
-contract PicoloNodes is Picolo {
-
+    // todo: TCR for masternodes
 }
--- dapp/truffle.js
+++ dapp/truffle.js
@@ -6,6 +6,12 @@ module.exports = {
   		host: "127.0.0.1",
   		port: "7545",
   		network_id: "*" // match all
+  	},
+  	ethermint: {
+		host: "0.0.0.0",
+		port: "8545",
+		network_id: "*", // match all
+  		from: "0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef"
   	}
   }
 };
