8d37b9652e06c8e82494a7a159172956f0280da4
Adi Kancherla <adikancherla@google.com>
2018-04-27 12:58:24 -0700

    removed

--- docker/truffle/migrations/1_initial_migration.js
+++
@@ -1,7 +0,0 @@
-var Migrations = artifacts.require("Migrations");
-var Picolo = artifacts.require("Picolo");
-
-module.exports = function(deployer, network, accounts) {
-  deployer.deploy(Migrations);
-  deployer.deploy(Picolo);
-};
